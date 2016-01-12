8ba3238892aa4b7dbc9162d068a973011c7c40d8
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-12 11:32:02 -0800

    Add different background colors to buttons with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-  $("button.btn").click(function(){
+  $("button#cats").click(function(){
+    $("body").removeClass();
+    $("body").addClass("cat-background");
     $("ul#cat").append('<li>Meow!<br><img src="img/cat.gif"></li>');
     $("ul#cat").children("li").click(function(){
       (this).remove();
     });
   });
-  $("button.btn").click(function(){
+  $("button#dogs").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dog-background");
     $("ul#dog").append('<li>Bark!<br><img src="img/dog.gif"></li>');
     $("ul#dog").children("li").click(function(){
       (this).remove();
     });
   });
+
+    $("button#orgin").click(function(){
+      $("body").removeClass();
+      $("body").addClass("body");
+    });
 });
